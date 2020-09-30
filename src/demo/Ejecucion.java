5098e55b41f7b4f1ad5f8da4cc09b27acce8daa2
freddy fernandez <fernandezyc@hotmail.com>
2020-09-29 19:36:20 -0500

    "primer commit de archivos transferidos al staging"

---
+++ src/demo/Ejecucion.java
@@ -0,0 +1,17 @@
+package demo;
+
+public class Ejecucion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x=1,y=2,z=3;
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		
+
+	}
+
+}
